4467083d05cf9a7874810149af75ddb940c4c13b
Elie <elieelaaraj50@gmail.com>
2025-06-18 20:53:38 +0300

    Remove some files

--- gaz_graphql/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class GazGraphQL(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gaz_graphql'
--- item/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ItemDetailView, SourceDetailView
-
-urlpatterns = [
-    path('', ItemDetailView.as_view(), name='add-item'),
-    path('<int:pk>/', ItemDetailView.as_view(), name='edit-item'),
-    path('source/', SourceDetailView.as_view(), name='add-source'),
-    path('source/<int:pk>/', SourceDetailView.as_view(), name='edit-source'),
-]
